d2cc595277c6d2d8d67e0aa77c498d7c1759194b
cuteseaurchin <cuteseaurchin@github.com>
2018-07-02 23:25:27 +0800

    Base character. Bug: spawning two mines

---
+++ Assets/CharacterBehaviorModules/AbilityCastingScripts/BaseAbilityCastingBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseAbilityCastingBehaviour : AbstractAbilityCastingBehaviour {
+
+    protected override void CastLightAttack(object castState)
+    {
+        base.DefaultCastLightAttack(castState);
+    }
+
+    protected override void CastHeavyAttack(object castState)
+    {
+        base.DefaultCastHeavyAttack(castState);
+    }
+
+    protected override void CastUtilityAbility(object castState)
+    {
+        GameObject mine = Instantiate(utilityAbility, abilitySpawnLocation, Quaternion.identity);
+        gameObject.transform.root.gameObject.BroadcastMessage("AbilityCasted", mine);
+        mine.GetComponent<CasterReference>().SetCaster(transform.root.gameObject);
+        //base.DefaultCastUtilityAbility(castState);
+    }
+}
