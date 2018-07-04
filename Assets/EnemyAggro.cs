23f2277b8b12ef4efa211bf48363c21c7fc4445a
cuteseaurchin <cuteseaurchin@github.com>
2018-07-04 09:43:47 +0800

    Enemy chases player in straight line

---
+++ Assets/EnemyAggro.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAggro : MonoBehaviour {
+    public Transform Player;
+    public int detectRange;
+    public int chaseSpeed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Calculate distance between player and enemy
+        float distance = Vector3.Distance(transform.position, Player.position);
+        //If player is within detection range, aggro enemy
+        if(distance <= detectRange)
+        {
+            //Make enemy face in player's direction
+            transform.LookAt(Player);
+            //Move towards player
+            Vector3 targetPosition = Player.transform.position;
+            transform.position += transform.forward * chaseSpeed * Time.deltaTime;
+        }
+    }
+}
---
+++ Assets/EnemyChase.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyChase : MonoBehaviour {
+    public int chaseSpeed;
+    public Transform Player;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Make enemy face in player's direction
+        transform.LookAt(Player);
+        //Move towards player
+        Vector3 targetPosition = Player.transform.position;
+        transform.position += transform.forward * chaseSpeed * Time.deltaTime;
+        //transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref chaseSpeed, smoothTime);
+    }
+}
