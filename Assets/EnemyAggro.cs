097cc8c14e1db4c84625f3a1ef648b82ac553a38
cuteseaurchin <cuteseaurchin@github.com>
2018-07-08 22:46:58 +0800

    Init and drop colliders

--- Assets/EnemyAggro.cs
+++ Assets/EnemyAggro.cs
@@ -3,51 +3,107 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyAggro : MonoBehaviour {
-    public Transform player;
-    public Transform enemyDetectStart;
-    public Transform enemyDetectEnd;
-    public int detectRange;
-    public int chaseSpeed;
+    public float chaseSpeed;
+    public float chaseTime;
+    private float originalChaseTime;
+    public bool initiated;
+    private bool prevInitiated;
+    public bool dropped;
+    private bool prevDropped;
     public bool detected = false;
     public Vector2 playerDirection;
-	// Use this for initialization
-	void Start () {
-        enemyDetectStart.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
-        enemyDetectEnd.position = new Vector3(enemyDetectStart.position.x + detectRange ,enemyDetectStart.position.y, 0);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        Raycasting();
-        if(detected)
+    private bool timerRunning = false;
+    private GameObject aggroInit;
+    private GameObject aggroDrop;
+    private AggroInit initScript;
+    private AggroDrop dropScript;
+    private GameObject player;
+
+    void Awake()
+    {
+    }
+
+    // Use this for initialization
+    void Start () {
+        initiated = false;
+        dropped = false;
+        originalChaseTime = chaseTime;
+        //Set previous to opposite of starting states
+        prevInitiated = !initiated;
+        prevDropped = !dropped;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Reset timer
+        timerRunning = true;        
+        aggroInit = GameObject.Find("AggroInit");
+        aggroDrop = GameObject.Find("AggroDrop");
+        initScript = aggroInit.GetComponent<AggroInit>();
+        dropScript = aggroDrop.GetComponent<AggroDrop>();        
+        //Update initiated and dropped variables
+        initiated = initScript.initiated;
+        dropped = dropScript.dropped;
+        Debug.Log(initiated);
+        //Debug.Log(dropped);
+        //If both initiated and dropped are true, player is no longer spotted but enemy still chases player
+        if (initiated && dropped)
         {
-            //Make enemy face in player's direction
-            transform.LookAt(player);
-            //Move towards player
-            Vector3 targetPosition = player.transform.position;
-            transform.position += transform.forward * chaseSpeed * Time.deltaTime;
+            //Set detected to true
+            detected = true;            
+            //Chase player for certain amount of time
+            if (timerRunning)
+            {
+                chaseTime -= Time.smoothDeltaTime;
+                if (chaseTime >= 0)
+                {
+                    //Chase player
+                    ChasePlayer();
+                }
+                else
+                {        
+                    //Once timer runs out, player is no longer detected                    
+                    detected = false;
+                    initiated = false;
+                    initScript.initiated = false;
+                    timerRunning = false;
+                }
+            }
+        }
+        //If only initiated is true, player is still spotted
+        else if(initiated && !dropped)
+        {
+            chaseTime = originalChaseTime;
+            //Set detected to true
+            detected = true;
+            //Enemy chases player
+            ChasePlayer();
+        }
+        //If only dropped is true, player is not spotted and enemy stops chasing
+        else if(!initiated && dropped)
+        {
+            chaseTime = originalChaseTime;
+            //Set initiated to false
+            initiated = false;
+            initScript.initiated = false;
+            //Set detected to false
+            detected = false;
+            //Enemy goes back to normal routine
+
         }
     }
 
-    void Raycasting()
+    void ChasePlayer()
     {
-        detected = Physics2D.Linecast(enemyDetectStart.position, enemyDetectEnd.position, 1 << LayerMask.NameToLayer("Player"));
-        /*
-        if (Physics2D.Linecast(enemyDetectStart.position, enemyDetectEnd.position, 1 << LayerMask.NameToLayer("Player")))
-        {
-            //if()
-            //{
-                detected = true;
-           // }
-           // else
-            //{
-                detected = false;
-            //}
-        }
-        else
-        {
-            detected = false;
-        }
-        */
+        //Get the player
+        player = GameObject.FindGameObjectWithTag("Player");
+        //Set player direction variable
+        playerDirection = player.transform.position - transform.position;
+        //Make enemy look at player
+        transform.parent.parent.LookAt(player.transform);
+        //Move enemy forward, since enemy is looking at player from previous line, moving enemy forward
+        //results in movement towards player
+        transform.parent.parent.position += transform.parent.parent.forward * chaseSpeed * Time.deltaTime;
     }
 }
