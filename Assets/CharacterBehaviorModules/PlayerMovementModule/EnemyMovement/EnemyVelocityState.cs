3de3b4f6f9a7f5e5fdf3c93f59c83ea1682eca50
cuteseaurchin <cuteseaurchin@github.com>
2018-07-12 17:02:30 +0800

    Figures out patrol route with ray

--- Assets/CharacterBehaviorModules/PlayerMovementModule/EnemyMovement/EnemyVelocityState.cs
+++ Assets/CharacterBehaviorModules/PlayerMovementModule/EnemyMovement/EnemyVelocityState.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 
 public class EnemyVelocityState : AbstractCharacterDirectionState
 {
+    public float margin;
     private float horizontalAxisValue;
     private float tempHorizontal;
     public float freezeDuration;
-    public bool hit = false;
+    private bool hit = false;
     private CharacterState hitState;
     private object prevHealth;
+    private float minX;
+    private float maxX;
+    private RaycastHit2D hitInfo;
+    private LayerMask groundMask;
 
     private void Awake()
     {
@@ -18,13 +23,14 @@ public class EnemyVelocityState : AbstractCharacterDirectionState
         statesManager = GetComponentInParent<ICharacterStateManager>();
         hitState = new CharacterState(ConstantStrings.HIT_STATE, hit);
         statesManager.RegisterCharacterState(hitState.name, hitState);
+        groundMask = LayerMask.GetMask("Ground");
     }
 	// Use this for initialization
 	void Start () {
         prevHealth = statesManager.GetCharacterStateValue(ConstantStrings.CURRENT_HEALTH);
         CharacterState.CharacterStateSubscription healthStateSubscription = statesManager.GetCharacterStateSubscription(ConstantStrings.CURRENT_HEALTH);
         healthStateSubscription.OnStateChanged += CheckHit;
-        horizontalAxisValue = 1;
+        horizontalAxisValue = 1;        
     }
 	
 	// Update is called once per frame
@@ -37,14 +43,25 @@ public class EnemyVelocityState : AbstractCharacterDirectionState
         {
             verticalValues = 0;
 		} else
-        //If enemy is not standing on the ground, turn on gravity
+        //If enemy is not standing on the ground, turn on gravity and find out bounds of floor under it
         if (!isGrounded)
         {
             verticalValues = -1;
-        }      
-        if(hit)
+            CastRay();
+        }
+        //If enemy has reached left bound of floor collider
+        if(transform.root.position.x <= minX + margin)
         {
-            FreezeStart(freezeDuration);           
+            horizontalAxisValue = 1;            
+        }
+        //If enemy has reached right bound of floor collider
+        else if (transform.root.position.x >= maxX - margin)
+        {
+            horizontalAxisValue = -1;
+        }
+        if (hit)
+        {
+            FreezeStart(freezeDuration);
         }
         directionState.SetState(new float[] { horizontalAxisValue, verticalValues });
     }
@@ -63,7 +80,6 @@ public class EnemyVelocityState : AbstractCharacterDirectionState
     {
         tempHorizontal = horizontalAxisValue;
         horizontalAxisValue = 0;
-        //Debug.Log("Freeze started");
         StartCoroutine(FreezeDuration(duration));
         hitState.SetState(false);
     }
@@ -71,16 +87,17 @@ public class EnemyVelocityState : AbstractCharacterDirectionState
     IEnumerator FreezeDuration(float duration)
     {
         yield return new WaitForSeconds(duration);
-        //Debug.Log("Freeze ended");
         horizontalAxisValue = tempHorizontal;
     }
 
-    void OnCollisionEnter2D(Collision2D coll)
+    //Cast a ray from enemy directly downwards to get the bounds of the floor it is standing on
+    private void CastRay()
     {
-        if(coll.gameObject.tag == "PatrolBound")
-        {
-            horizontalAxisValue *= -1;
-            //Debug.Log("Bounds Hit");
-        }        
+        Vector2 startingPosition = new Vector2(transform.position.x, transform.position.y);
+        Vector2 direction = new Vector2(0,-1);
+        float rayLength = 10;   //Arbitrary ray length, can possibly be shorter
+        hitInfo = Physics2D.Raycast(startingPosition, direction, rayLength, groundMask);
+        minX = hitInfo.collider.bounds.min.x;
+        maxX = hitInfo.collider.bounds.max.x;
     }
 }
