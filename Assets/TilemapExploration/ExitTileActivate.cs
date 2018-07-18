7ec2cd8ac997f17918561c939d4cd0e3cad5a13b
cuteseaurchin <cuteseaurchin@github.com>
2018-07-18 22:49:54 +0800

    Boss Scenes
    BossFight Scene Minor

---
+++ Assets/TilemapExploration/ExitTileActivate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitTileActivate : MonoBehaviour {
+    private GameObject loadingScreen;
+    void Start()
+    {
+        loadingScreen = GameObject.Find("Loading Screen");
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col != null)
+        {
+            if (col.gameObject.tag == "Player")
+            {
+                //GetComponent<GlobalGameObject>().LoadGameScene("BossFight");
+                GameObject globalObject = GameObject.Find("GlobalGameObject") as GameObject;
+                //globalObject.GetComponentInChildren<GlobalGameObject>().DisplayUIElement(loadingScreen);
+                globalObject.GetComponentInChildren<GlobalGameObject>().LoadGameScene("BossFight").allowSceneActivation = true;
+                
+            }
+        }        
+    }    
+}
