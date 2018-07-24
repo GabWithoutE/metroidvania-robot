abf1bd39f77a76fca1bdb55644fc0eef6d773942
Gabriel Chen <gabriel.luke.chen@gmail.com>
2018-07-24 12:09:10 -0700

    TastyEffects TODO:
    1. Camera follow player appropriately.
    	stops following at the end of the screen.
    2. Camera Shakes on hits
    	player hits enemy (tiny shake)
    	enemy hits player (large shake)
    	player lands on ground hard (big shake)
    3. Hit-pause
    	player gets hit, big pause
    	player kills an enemy, small pause
    4. Hit effects
    	spawn effects when player gets hit
    	spawn effects when player hits enemy
    
    camera raycast InProgress...

---
+++ Assets/CameraMovement/CameraPlayerFollow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPlayerFollow : MonoBehaviour {
+	public GameObject player;
+	private int screenEdgeMask;
+	private void Awake()
+	{
+		screenEdgeMask = LayerMask.NameToLayer("ScreenEdge");
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -10);
+		/*
+         * TODO do raycast from player position to check if i'm hittin the edge of the map, stop the camera. 
+         */
+		Debug.DrawRay(player.transform.position, Vector3.right, Color.blue);
+		RaycastHit2D hitScreenEdge = Physics2D.Raycast(player.transform.position, Vector2.right, 10, screenEdgeMask);
+	}
+}
