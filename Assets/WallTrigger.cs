fc0a68d2a62e6ec228d60305cf9fd1f78771c1da
cuteseaurchin <38171359+cuteseaurchin@users.noreply.github.com>
2018-07-24 12:43:46 +0800

    Boss Scenes (#15)
    BossFight Scene Minor

---
+++ Assets/WallTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallTrigger : MonoBehaviour {
+    private GameObject bossWall;
+	// Use this for initialization
+	void Start () {
+        bossWall = GameObject.Find("Grid").transform.Find("BossWall").gameObject;
+        bossWall.SetActive(false);
+	}	
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            bossWall.SetActive(true);
+        }
+    }
+}
