05d5670e9cfd7183d3a271c3065b2aa66e3fd751
Gabriel Chen <gabriel.luke.chen@gmail.com>
2018-07-25 21:18:37 -0700

    attempt 2

--- Assets/CameraMovement/CameraPlayerFollow.cs
+++ Assets/CameraMovement/CameraPlayerFollow.cs
@@ -4,81 +4,254 @@ using UnityEngine;
 
 public class CameraPlayerFollow : MonoBehaviour {
 	public GameObject player;
-	public float horizontalDistanceToMapEdge;
-	public float verticalDistanceToMapEdge;
-	public float cameraVerticalOffset;
-	private LayerMask mapEdgeLayerMask;
-	private Vector3 cameraPosition;
+    /*
+     * Used when the camera is moving independently of the exact movement of the player;
+     */ 
+	public float cameraLagSpeed;
+    /*
+     * Used to pan the camera slightly up when jumping and when the vertical input axis is being pressed;
+     */ 
+	public float defaultTopLineDistance;
+    /*
+     * Used to offset the player from the center of the screen in default situations.
+     */ 
+	public float cameraVerticalOffsetFromBottomRatio;
+	private LayerMask cameraDirectionMask;
+	private Vector3 newCameraPosition;
+
+	private float cameraDistanceToVerticalEdge;
+	private float cameraDistanceToHorizontalEdge;
+	private float cameraHeight;
+	private float cameraWidth;
+
+	private float desiredCameraYPosition;
+	private float screenBottom;
+	private float characterBellowThisHeight;
+	private ICharacterStateObserver playerStateObserver;
+
+	private Vector3 cameraViewportPosition;
+	private Vector3 cameraViewportTopRightCornerPosition;
+	private Vector3 cameraViewportBottomLeftCornerPosition;
+
+
+	private bool topLineHit;
+	private float playerYVelocityStateObservation;
+	private float topLineyPosition;
+	private string topLineStrategy;
+
+	public float fallingLagDistance;
+	public float fallingLagDistanceIncreaseRate;
+	private float currentFallingLagDistance;
+
+	private float bottomLineYPosition;
+    /*
+     * TODO:
+     * if the player exceeds the vertical offset distance, then do the slight camera vertical pan, then when player lands pan back.
+     */ 
 
 	private void Awake()
 	{
-		mapEdgeLayerMask = LayerMask.GetMask("MapEdge");
-		cameraPosition = new Vector3(0, 0, transform.position.z);
+		topLineStrategy = "";
+		topLineHit = false;
+		playerStateObserver = player.GetComponent(typeof(ICharacterStateObserver)) as ICharacterStateObserver;
+		SetScreenDistances();
+		currentFallingLagDistance = 0;
 	}
+
+	private void SetScreenDistances(){
+		cameraViewportPosition = new Vector3(0.5f, 0.5f, transform.position.z);
+        cameraViewportTopRightCornerPosition = new Vector3(0f, 0f, transform.position.z);
+        cameraViewportBottomLeftCornerPosition = new Vector3(1f, 1f, transform.position.z);
+
+        cameraDirectionMask = LayerMask.GetMask("CameraDirections");
+        newCameraPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        Vector3 topRightCornerPosition = Camera.main.ViewportToWorldPoint(cameraViewportTopRightCornerPosition);
+        Vector3 middlePosition = Camera.main.ViewportToWorldPoint(cameraViewportPosition);
+        Vector3 bottomRightCornerPosition = Camera.main.ViewportToWorldPoint(cameraViewportBottomLeftCornerPosition);
+
+        cameraDistanceToVerticalEdge = topRightCornerPosition.y - middlePosition.y;
+        cameraDistanceToHorizontalEdge = topRightCornerPosition.x - middlePosition.x;
+        cameraHeight = topRightCornerPosition.y - bottomRightCornerPosition.y;
+        cameraWidth = topRightCornerPosition.x - bottomRightCornerPosition.x;
+	}
+
 	// Use this for initialization
 	void Start () {
-		
+		playerStateObserver.GetCharacterStateSubscription(ConstantStrings.GROUNDED).OnStateChanged += CalculateTopLineOnGrounded;	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-
+		playerYVelocityStateObservation = ((float[])playerStateObserver.GetCharacterStateValue(ConstantStrings.VELOCITY))[1];
+		newCameraPosition = transform.position;
+		newCameraPosition.x = player.transform.position.x;
 		/*
-         * TODO do raycast from player position to check if i'm hittin the edge of the map, stop the camera. 
-         */
-        
-		if(!CastRaysHorizontal()){
-			cameraPosition.x = player.transform.position.x;
-			//transform.position = new Vector3(player.transform.position.x, player.transform.position.y + cameraVerticalOffset, transform.position.z);
+		 * cast raycast and collect all hits
+		 * if director is available, use the director's strategy, if not, use the default strategy
+		 */ 
+		RaycastHit2D[] hitCameraDirectors =
+			Physics2D.RaycastAll(player.transform.position, Vector2.up, 1, cameraDirectionMask);
+		Debug.DrawRay(player.transform.position, Vector2.up, Color.blue);
+		if (hitCameraDirectors.Length > 0){
+			foreach(RaycastHit2D director in hitCameraDirectors){
+				FollowCameraDirection(director.collider.gameObject);
+			}
 		} else {
-			cameraPosition.x = transform.position.x;
+			DefaultCameraDirectionMovement();
 		}
 
-		if (!CastRaysVertical()){
-			cameraPosition.y = player.transform.position.y + cameraVerticalOffset;
+
+		if (playerYVelocityStateObservation == 0)
+        {
+			currentFallingLagDistance = 0;
+		    MoveLaggingCamera();
+		}
+		transform.position = newCameraPosition;
+	}
+
+	private void MoveLaggingCamera(){
+            if (newCameraPosition.y < desiredCameraYPosition)
+            {
+                newCameraPosition.y += Time.deltaTime * cameraLagSpeed;
+                newCameraPosition.y = Mathf.Clamp(newCameraPosition.y, float.MinValue, desiredCameraYPosition);
+            }
+            else if (newCameraPosition.y > desiredCameraYPosition)
+            {
+                newCameraPosition.y -= Time.deltaTime * cameraLagSpeed;
+                newCameraPosition.y = Mathf.Clamp(newCameraPosition.y, desiredCameraYPosition, float.MaxValue);
+            }
+	}
+    
+	private void FollowCameraDirection(GameObject director){
+		switch (director.name){
+			case ConstantStrings.CameraDirectionBoxes.STAGE_EDGE_HORIZONTAL_RIGHT:
+				DirectCameraStageEdgeHorizontalMovementRight(director.GetComponent<BoxCollider2D>());
+				break;
+			case ConstantStrings.CameraDirectionBoxes.STAGE_EDGE_HORIZONTAL_LEFT:
+				DirectCameraStageEdgeHorizontalMovementLeft(director.GetComponent<BoxCollider2D>());
+                break;
+			case ConstantStrings.CameraDirectionBoxes.STAGE_BOTTOM_LINE:
+				DirectCameraBottomLineMovement(director.GetComponent<BoxCollider2D>());
+				topLineStrategy = ConstantStrings.CameraDirectionBoxes.STAGE_BOTTOM_LINE;
+				break;
+			case ConstantStrings.CameraDirectionBoxes.FOCUS_ON_AREA:
+				DirectCameraFocusOnAreaMovement(director.GetComponent<BoxCollider2D>());
+				break;
+			default:
+				break;
+		}
+	}
+
+	private void CalculateTopLineOnGrounded(object groundedState){
+		bool isGrounded = ((bool[])groundedState)[1];
+		switch(topLineStrategy){
+			case ConstantStrings.CameraDirectionBoxes.STAGE_BOTTOM_LINE:
+				break;
+			default:
+				DefaultCalculateTopLine();
+				break;
+		}
+
+	}
+
+	//private void FromBottomLineCalculateTopLine(){
+	//	/*
+	//	 * use the bottom line to calculate the top line using the ratio
+	//	 * don't allow the camera to fall below the bottom line + the fall lag distance
+	//	 */ 
+	//	topLinePosition = bottomLineYPosition + (.5f - cameraVerticalOffsetFromBottomRatio) * cameraHeight;
+	//	print(topLinePosition);
+	//}
+
+    /*
+     * Calculates the default top line (slightly above the player);
+     */ 
+	private void DefaultCalculateTopLine(){
+		topLineyPosition = player.transform.position.y + defaultTopLineDistance; 
+	}
+
+	/*
+     * Directs the camera such that the bottom of the director's collider is used as the bottom line of the screen
+     */
+    private void DirectCameraBottomLineMovement(BoxCollider2D directorCollider)
+    {
+        bottomLineYPosition = directorCollider.bounds.min.y;
+		topLineyPosition = bottomLineYPosition + (cameraVerticalOffsetFromBottomRatio * cameraHeight);
+		//print(((.5f - cameraVerticalOffsetFromBottomRatio) * cameraHeight));
+		//print(topLinePosition);
+
+		Debug.DrawRay(new Vector3(transform.position.x, topLineyPosition, 0), Vector3.right * 3, Color.green);
+
+		if (playerYVelocityStateObservation != 0){
+			if (playerYVelocityStateObservation > 0)
+			{   
+				if (newCameraPosition.y == desiredCameraYPosition){
+					if (player.transform.position.y > topLineyPosition)
+                    {
+                        newCameraPosition.y = player.transform.position.y + (.5f - cameraVerticalOffsetFromBottomRatio) * cameraHeight;
+                        print(((.5f - cameraVerticalOffsetFromBottomRatio) * cameraHeight));
+
+                    }
+				} else {
+					if(player.transform.position.y >= newCameraPosition.y){
+						newCameraPosition.y = player.transform.position.y + (.5f - cameraVerticalOffsetFromBottomRatio) * cameraHeight;
+					}
+				}
+
+			} else {
+				if (player.transform.position.y > topLineyPosition)
+                {
+					currentFallingLagDistance += Time.deltaTime * fallingLagDistanceIncreaseRate;
+                    currentFallingLagDistance = Mathf.Clamp(currentFallingLagDistance, 0, fallingLagDistance);
+                    newCameraPosition.y = player.transform.position.y
+                        + (.5f - cameraVerticalOffsetFromBottomRatio) * cameraHeight + currentFallingLagDistance;
+					newCameraPosition.y = Mathf.Clamp(newCameraPosition.y,
+					    bottomLineYPosition + (.5f - cameraVerticalOffsetFromBottomRatio) * cameraHeight + fallingLagDistance, float.MaxValue);
+                }
+			}
 		} else {
-			cameraPosition.y = transform.position.y;
+			desiredCameraYPosition = bottomLineYPosition + cameraDistanceToVerticalEdge;
 		}
-		transform.position = cameraPosition;
+    }
+	private void DefaultCameraDirectionMovement(){
+		if (playerYVelocityStateObservation != 0){
+			if (playerYVelocityStateObservation > 0){
+				newCameraPosition.y = player.transform.position.y+ (.5f - cameraVerticalOffsetFromBottomRatio) * cameraHeight;
+			} else {
+				currentFallingLagDistance += Time.deltaTime * fallingLagDistanceIncreaseRate;
+				currentFallingLagDistance = Mathf.Clamp(currentFallingLagDistance, 0, fallingLagDistance);
+				newCameraPosition.y = player.transform.position.y 
+					+ (.5f - cameraVerticalOffsetFromBottomRatio) * cameraHeight + currentFallingLagDistance;
+			}
 
-        /*
-         * TODO:
-         * Move camera to certain areas. Do different Camera movement types based on colliders.
-         */ 
-
+		} else{
+			desiredCameraYPosition = player.transform.position.y + (.5f - cameraVerticalOffsetFromBottomRatio) * cameraHeight;
+		}
 	}
 
-	private bool CastRaysHorizontal(){
-		//RaycastHit2D hitStageEdgeRight = Physics2D.Raycast(player.transform.position, Vector2.right, horizontalDistanceToMapEdge, mapEdgeMask);
-		RaycastHit2D hitStageEdgeRight = Physics2D.Raycast(player.transform.position, Vector2.right, horizontalDistanceToMapEdge, mapEdgeLayerMask);
 
-		Debug.DrawRay(player.transform.position, Vector2.right * horizontalDistanceToMapEdge, Color.blue);
-		if (hitStageEdgeRight){
-			return true;
-		}
-		RaycastHit2D hitStageEdgeLeft = Physics2D.Raycast(player.transform.position, Vector2.left, horizontalDistanceToMapEdge, mapEdgeLayerMask);
-
-		Debug.DrawRay(player.transform.position, Vector2.left * horizontalDistanceToMapEdge, Color.blue);
-		if (hitStageEdgeLeft){
-			return true;
-		}
-
-		return false;
+    /*
+     * Directs the camera s.t. the camera no longer centers the player at the edge of a stage
+     */
+	private void DirectCameraStageEdgeHorizontalMovementRight(BoxCollider2D directorCollider){
+		
 	}
 
-	private bool CastRaysVertical(){
-		RaycastHit2D hitStageUp = Physics2D.Raycast(player.transform.position, Vector2.up, verticalDistanceToMapEdge, mapEdgeLayerMask);
-		Debug.DrawRay(player.transform.position, Vector2.up * verticalDistanceToMapEdge, Color.blue);
-		if (hitStageUp){
-			return true;
-		}
+	/*
+     * Directs the camera s.t. the camera no longer centers the player at the edge of a stage
+     */
+    private void DirectCameraStageEdgeHorizontalMovementLeft(BoxCollider2D directorCollider)
+    {
+		float desiredXPosition = directorCollider.bounds.max.x;
+		newCameraPosition.x = desiredXPosition;
+    }
 
-		RaycastHit2D hitStageDown = Physics2D.Raycast(player.transform.position, Vector2.down, verticalDistanceToMapEdge, mapEdgeLayerMask);
-		Debug.DrawRay(player.transform.position, Vector2.down * verticalDistanceToMapEdge, Color.blue);
-		if (hitStageDown){
-			return true;
-		}
-		return false;
+    /*
+     * Directs the camera s.t. the camera moves to the desired location when the player enters a contained 
+     * no camera movement area
+     */ 
+	private void DirectCameraFocusOnAreaMovement(BoxCollider2D directorCollider){
+		
 	}
+
 }
--- Assets/ConstantStrings.cs
+++ Assets/ConstantStrings.cs
@@ -2,53 +2,59 @@ using UnityEngine;
 using System.Collections;
 public static class ConstantStrings
 {
-    public static readonly string TIME_SCALE = "TimeScale";
-    public static readonly string CARDINAL_DIRECTION = "CardinalDirection";
-	public static readonly string TRANSFORMATION = "Transformation";
-	public static readonly string VELOCITY = "Velocity";
-	public static readonly string SPEED_SCALE = "SpeedScale";
+	public const string TIME_SCALE = "TimeScale";
+    public const string CARDINAL_DIRECTION = "CardinalDirection";
+	public const string TRANSFORMATION = "Transformation";
+	public const string VELOCITY = "Velocity";
+	public const string SPEED_SCALE = "SpeedScale";
 
-    //public static readonly string MOVE_SET = "MoveSet";
-    public static readonly string CURRENT_HEALTH = "CurrentHealth";
-    public static readonly string MAX_HEALTH = "MaxHealth";
-    public static readonly string DEATH_STATE = "DeathState";
-    public static readonly string HIT_STATE = "HitState";
+    //public const string MOVE_SET = "MoveSet";
+    public const string CURRENT_HEALTH = "CurrentHealth";
+    public const string MAX_HEALTH = "MaxHealth";
+    public const string DEATH_STATE = "DeathState";
+    public const string HIT_STATE = "HitState";
 
-    public static readonly string LIGHT_ATTACK_COOLDOWN = "LightAttackCooldown";
-	public static readonly string HEAVY_ATTACK_COOLDOWN = "HeavyAttackCooldown";
-	public static readonly string UTILITY_COOLDOWN = "UtilityCooldown";
+    public const string LIGHT_ATTACK_COOLDOWN = "LightAttackCooldown";
+	public const string HEAVY_ATTACK_COOLDOWN = "HeavyAttackCooldown";
+	public const string UTILITY_COOLDOWN = "UtilityCooldown";
 
-	public static readonly string MAINGAME_SCENE_NAME = "MainGameSceneForREa";
+	public const string MAINGAME_SCENE_NAME = "MainGameSceneForREa";
 
-	public static readonly string HIT_ENEMY = "HitEnemy";
-	public static readonly string KILLED_ENEMY = "KilledEnemy";
+	public const string HIT_ENEMY = "HitEnemy";
+	public const string KILLED_ENEMY = "KilledEnemy";
 
-	public static readonly string SCORE = "Score";
+	public const string SCORE = "Score";
 
-	public static readonly string JUMPING = "Jumping";
-	public static readonly string GROUNDED = "Grounded";
+	public const string JUMPING = "Jumping";
+	public const string GROUNDED = "Grounded";
 
-	public static readonly string DIRECTION = "Direction";
+	public const string DIRECTION = "Direction";
 
-	public static readonly string LIGHT_ATTACK_CAST = "LightAttackCast";
-	public static readonly string HEAVY_ATTACK_CAST = "HeavyAttackCast";
-	public static readonly string UTILITY_ABILITY_CAST = "UtilityAbilityCast";
+	public const string LIGHT_ATTACK_CAST = "LightAttackCast";
+	public const string HEAVY_ATTACK_CAST = "HeavyAttackCast";
+	public const string UTILITY_ABILITY_CAST = "UtilityAbilityCast";
 
-	public static readonly string UTILITY_RESOURCE_STATE = "UtilityResourceState";
+	public const string UTILITY_RESOURCE_STATE = "UtilityResourceState";
 
+	public static class CameraDirectionBoxes{
+		public const string STAGE_EDGE_HORIZONTAL_RIGHT = "StageEdgeHorizontalRight";
+		public const string STAGE_EDGE_HORIZONTAL_LEFT = "StageEdgeHorizontalLeft";
+		public const string STAGE_BOTTOM_LINE = "StageBottomLine";
+		public const string FOCUS_ON_AREA = "FocusOnArea";
+	}
     
 	public static class PlayerAnimatorStates{
-		public static readonly string IDLE_HORIZONTAL = "IdleHorizontal";
-		public static readonly string IDLE_VERTICAL = "IdleVertical";
+		public const string IDLE_HORIZONTAL = "IdleHorizontal";
+		public const string IDLE_VERTICAL = "IdleVertical";
 
-		public static readonly string LIGHT_ATTACK_ANIMATION = "LightAttackAnimation";
-		public static readonly string HEAVY_ATTACK_ANIMATION = "HeavyAttackAnimation";
-		public static readonly string UTILITY_ABILITY_ANIMATION = "UtilityAbilityAnimation";
+		public const string LIGHT_ATTACK_ANIMATION = "LightAttackAnimation";
+		public const string HEAVY_ATTACK_ANIMATION = "HeavyAttackAnimation";
+		public const string UTILITY_ABILITY_ANIMATION = "UtilityAbilityAnimation";
 
-		public static readonly string RUNNING_HORIZONTAL = "RunningHorizontal";
-        public static readonly string RUNNING_VERTICAL = "RunningVertical";
+		public const string RUNNING_HORIZONTAL = "RunningHorizontal";
+        public const string RUNNING_VERTICAL = "RunningVertical";
 
-		public static readonly string AWAY = "Away";
+		public const string AWAY = "Away";
 	}
     
 
@@ -57,19 +63,19 @@ public static class ConstantStrings
     {
         public static class Input
         {
-            public static readonly string INPUT_HORIZONTAL = "InputHorizontal";
-			public static readonly string INPUT_VERTICAL = "InputVertical";
-            public static readonly string INPUT_LIGHT_ATTACK = "InputLightAttack";
-			public static readonly string INPUT_HEAVY_ATTACK = "InputHeavyAttack";
-			public static readonly string INPUT_UTILITY = "InputUtility";
-			public static readonly string INPUT_TRANSFORM = "InputTransform";
-			public static readonly string INPUT_JUMP = "InputJump";
+            public const string INPUT_HORIZONTAL = "InputHorizontal";
+			public const string INPUT_VERTICAL = "InputVertical";
+            public const string INPUT_LIGHT_ATTACK = "InputLightAttack";
+			public const string INPUT_HEAVY_ATTACK = "InputHeavyAttack";
+			public const string INPUT_UTILITY = "InputUtility";
+			public const string INPUT_TRANSFORM = "InputTransform";
+			public const string INPUT_JUMP = "InputJump";
         }
 
         public static class HUD
         {
-            public static readonly string HEALTH_BAR = "HealthBar";
-            public static readonly string PROFILE_PICTURE = "ProfilePicture";
+            public const string HEALTH_BAR = "HealthBar";
+            public const string PROFILE_PICTURE = "ProfilePicture";
         }
 
     }
@@ -78,15 +84,15 @@ public static class ConstantStrings
     {
         public static class HammerBoss
         {
-            public static readonly string HAMMER_THROWN = "HammerThrown";
-            public static readonly string HAMMER_HITS_GROUND = "HammerHitsGround";
-            public static readonly string HAMMER_HIT_PLAYER = "HammerHitPlayer";
-            public static readonly string HAMMER_HIT_ENEMY = "HammerHitEnemy";
-            public static readonly string THROWN_HAMMER_POSITION = "ThrownHammerPosition";
-            public static readonly string MELEE_ATTACK_CAST_STATE = "meleeAttackCastState";
-            public static readonly string HAMMER_THROW_CAST_STATE = "hammerThrowCastState";
-            public static readonly string JUMP_STATE = "jumpState";
-            public static readonly string BUSY_STATE = "busyState";
+            public const string HAMMER_THROWN = "HammerThrown";
+            public const string HAMMER_HITS_GROUND = "HammerHitsGround";
+            public const string HAMMER_HIT_PLAYER = "HammerHitPlayer";
+            public const string HAMMER_HIT_ENEMY = "HammerHitEnemy";
+            public const string THROWN_HAMMER_POSITION = "ThrownHammerPosition";
+            public const string MELEE_ATTACK_CAST_STATE = "meleeAttackCastState";
+            public const string HAMMER_THROW_CAST_STATE = "hammerThrowCastState";
+            public const string JUMP_STATE = "jumpState";
+            public const string BUSY_STATE = "busyState";
         }
     }
 
