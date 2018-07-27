4fecb534b0c865fa816e8a0ffb74a2ace0fb74bb
cuteseaurchin <cuteseaurchin@github.com>
2018-07-27 14:57:58 +0800

    working on boss animator

--- Assets/BossAnimationStates.cs
+++ Assets/BossAnimationStates.cs
@@ -3,12 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BossAnimationStates : MonoBehaviour {
+    private ICharacterStateObserver stateObserver;
     private Animator bossAnimator;
-    private int walkingHash = Animator.StringToHash("Walking");
-    private int jumpingHash = Animator.StringToHash("Jumping");
-    private int fallingHash = Animator.StringToHash("Falling");
-    private int meleeHash = Animator.StringToHash("Melee");
-    private int hammerHash = Animator.StringToHash("Hammer");
+    private int walkingHash;
+    private int jumpingHash;
+    private int fallingHash;
+    private int meleeHash;
+    private int hammerHash;
+
+    void Awake()
+    {
+        stateObserver = GetComponentInParent(typeof(ICharacterStateObserver)) as ICharacterStateObserver;
+        walkingHash = Animator.StringToHash("Walking");
+        jumpingHash = Animator.StringToHash("Jumping");
+        fallingHash = Animator.StringToHash("Falling");
+        meleeHash = Animator.StringToHash("Melee");
+        hammerHash = Animator.StringToHash("Hammer");
+    }
 
     // Use this for initialization
     void Start () {
