8d6cdb6063b2d116d3b199c38a2b132c903db989
cuteseaurchin <cuteseaurchin@github.com>
2018-07-27 08:26:55 +0800

    animator controller in progress

---
+++ Assets/BossAnimationStates.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossAnimationStates : MonoBehaviour {
+    private Animator bossAnimator;
+    private int walkingHash = Animator.StringToHash("Walking");
+    private int jumpingHash = Animator.StringToHash("Jumping");
+    private int fallingHash = Animator.StringToHash("Falling");
+    private int meleeHash = Animator.StringToHash("Melee");
+    private int hammerHash = Animator.StringToHash("Hammer");
+
+    // Use this for initialization
+    void Start () {
+        bossAnimator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
