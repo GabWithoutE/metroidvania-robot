ea637bb5c2f201181555dfbd7fdf9a9022aa5d8b
cuteseaurchin <cuteseaurchin@github.com>
2018-07-27 15:19:37 +0800

    working on animator controller

--- Assets/BossAnimationStates.cs
+++ Assets/BossAnimationStates.cs
@@ -24,10 +24,38 @@ public class BossAnimationStates : MonoBehaviour {
     // Use this for initialization
     void Start () {
         bossAnimator = GetComponent<Animator>();
-	}
+        CharacterState.CharacterStateSubscription hammerThrowSub = stateObserver.GetCharacterStateSubscription(ConstantStrings.Enemy.HammerBoss.HAMMER_THROW_CAST_STATE);
+        hammerThrowSub.OnStateChanged += OnHammerThrow;
+        CharacterState.CharacterStateSubscription meleeAttackSub = stateObserver.GetCharacterStateSubscription(ConstantStrings.Enemy.HammerBoss.MELEE_ATTACK_CAST_STATE);
+        meleeAttackSub.OnStateChanged += OnMeleeAttack;
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    private void OnHammerThrow(object hammerThrowCastState)
+    {
+        if((bool)hammerThrowCastState)
+        {
+            bossAnimator.SetBool(hammerHash, true);
+        }
+        else
+        {
+            bossAnimator.SetBool(hammerHash, false);
+        }
+    }
+
+    private void OnMeleeAttack(object meleeAttackCastState)
+    {
+        if((bool)meleeAttackCastState)
+        {
+            bossAnimator.SetBool(meleeHash, true);
+        }
+        else
+        {
+            bossAnimator.SetBool(meleeHash, false);
+        }
+    }
 }
