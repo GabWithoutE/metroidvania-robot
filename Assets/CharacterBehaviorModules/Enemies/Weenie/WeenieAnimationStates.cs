3b66101dfde1ccf356048c03a61ced51c8da0b2f
cuteseaurchin <cuteseaurchin@github.com>
2018-07-27 16:11:04 +0800

    Finished but not tested yet

---
+++ Assets/CharacterBehaviorModules/Enemies/Weenie/WeenieAnimationStates.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeenieAnimationStates : MonoBehaviour {
+    private Animator weenieAnimator;
+    private ICharacterStateObserver stateObserver;
+    private int aggroedHash;
+    private bool aggroed;
+
+    void Awake()
+    {
+        weenieAnimator = GetComponent<Animator>();
+        stateObserver = GetComponentInParent(typeof(ICharacterStateObserver)) as ICharacterStateObserver;
+        aggroedHash = Animator.StringToHash("Aggroed");
+    }
+
+    // Use this for initialization
+    void Start () {
+        aggroed = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //If weenie is aggroed, set animation parameter to true
+		if(aggroed)
+        {
+            weenieAnimator.SetBool(aggroedHash, true);
+        }
+        else
+        {
+            weenieAnimator.SetBool(aggroedHash, false);
+        }
+	}
+}
