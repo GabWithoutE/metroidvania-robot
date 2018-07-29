f01180d3962079f3de48a522b37d739961aa87bb
cuteseaurchin <cuteseaurchin@github.com>
2018-07-29 22:56:30 +0800

    Error with cameraplayerfollow

---
+++ Assets/TilemapExploration/Scene2To1.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scene2To1 : MonoBehaviour {
+    private GameObject loadingScreen;
+    void Start()
+    {
+        loadingScreen = GameObject.Find("Loading Screen");
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col != null)
+        {
+            if (col.gameObject.tag == "Player")
+            {
+                //GetComponent<GlobalGameObject>().LoadGameScene("BossFight");
+                GameObject globalObject = GameObject.Find("GlobalGameObject") as GameObject;
+                //globalObject.GetComponentInChildren<GlobalGameObject>().DisplayUIElement(loadingScreen);
+                globalObject.GetComponentInChildren<GlobalGameObject>().LoadGameScene("SceneTransitions1").allowSceneActivation = true;
+                
+            }
+        }        
+    }    
+}
