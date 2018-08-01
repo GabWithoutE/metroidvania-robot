ba88eb719a15d487212441722d35a6878f1c3a5f
cuteseaurchin <cuteseaurchin@github.com>
2018-08-01 08:13:07 +0800

    Working on scene manager

---
+++ Assets/DestroyIfMultiple.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyIfMultiple : MonoBehaviour {
+    void Awake()
+    {
+        //If there is already an instance of a scene manager, desroy this one
+        if (FindObjectsOfType(GetType()).Length > 1)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/StageEnemies.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageEnemies : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //Instantiates a weenie enemy at input position
+    public void placeEnemy(Vector2 position)
+    {
+
+    }
+}
