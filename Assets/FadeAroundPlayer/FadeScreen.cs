e8fc6f32e680798c9bd4ad3c47cbacb9ea4737e6
cuteseaurchin <38171359+cuteseaurchin@users.noreply.github.com>
2018-08-23 13:42:12 +0800

    Basic fading around player (#26)
    * Basic fading around player
    
    * Changed fading to use scale of image

---
+++ Assets/FadeAroundPlayer/FadeScreen.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeScreen : MonoBehaviour {
+    public CanvasGroup faderCanvasGroup;
+    private bool startFading = false;       //Boolean to figure out when player enteres area to fade screen
+    public GameObject player;               //Reference to player
+    private BoxCollider2D boxCollider;      //Collider to detect when player is in area to fade screen
+    private ChangeImageScale changeImageScale;  //Used to change the scale of the image
+    public float defaultScale;  //Default scale of the image
+    public float minScale;  //The minimum scale the image can have before the edge of the image becomes visible to player.
+
+
+    void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+        changeImageScale = faderCanvasGroup.GetComponentInChildren<ChangeImageScale>();
+        changeImageScale.ChangeScale(defaultScale);   //Default image scale
+        faderCanvasGroup.alpha = 1f;                    //Have image at full alpha always
+    }
+
+    //Calculates how much to fade image by and maps that value to a value between 0 and 1
+    private float CalculateFadeAmount()
+    {
+        //Calculates distance from player to center of boxcollider
+        float distancePlayerToCenter = Mathf.Abs(boxCollider.transform.position.x - player.transform.position.x);
+        //Clamps value between minimum scale and default scale
+        return Mathf.Clamp(((distancePlayerToCenter / boxCollider.bounds.extents.x)) * defaultScale, minScale, defaultScale);        
+    }
+
+    void Update()
+    {
+        //If player hits the collider, calculate how much to fade by
+        if(startFading)
+        {
+            changeImageScale.ChangeScale(CalculateFadeAmount());
+        }
+    }
+
+    //Only start fading when player hits the collider, otherwise don't fade or the math will not work
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            startFading = true;
+        }        
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            startFading = false;
+        }
+    }
+}
---
+++ Assets/FadeAroundPlayer/FollowPlayerPosition.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerPosition : MonoBehaviour {
+    private GameObject player;
+    private RectTransform rectTransform;
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+        rectTransform = GetComponent<RectTransform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rectTransform.transform.position = player.transform.position;
+	}
+}
