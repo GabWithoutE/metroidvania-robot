1edde0108e9b4a0d2ac15ebf006ee8492d320447
Gabriel Chen <gabriel.luke.chen@gmail.com>
2018-10-01 18:56:19 -0700

    Refactor and Magnet Hammer
    Reorganizing folders started.
    Started magnet hammer animations.

---
+++ Assets/Editor/InteractableEnvironment/SynchronizedEnvironmentPieces/SynchronizeEnvironmentPiecesEditor.cs
@@ -0,0 +1,50 @@
+﻿// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEditor;
+
+// [CustomEditor (typeof(SynchronizeEnvironmentPieces))]
+// public class SynchronizeEnvironmentPiecesEditor : Editor {
+// 	private SerializedProperty actionOffsetValuesProperty;
+// 	private SerializedProperty environmentPiecesProperty;
+
+// 	private const string envPiecePropOffsetValuesName = "environmentPiecesActionOffsetValues";
+// 	private const string envPiecePropPiecesName = "environmentPieces";
+
+// 	private bool[] showEnvironmentPieceSlots= new bool[2];
+
+// 	private void OnEnable() {
+// 		actionOffsetValuesProperty = 
+// 			serializedObject.FindProperty(envPiecePropOffsetValuesName);
+// 		environmentPiecesProperty = 
+// 			serializedObject.FindProperty(envPiecePropPiecesName);
+// 		Debug.Log(environmentPiecesProperty.arraySize);
+// 	}
+
+// 	public override void OnInspectorGUI(){
+// 		serializedObject.Update();
+
+// 		for (int i = 0; i < 2; i++){
+// 			EnvironmentPieceGUI(i);
+// 		}
+
+// 		serializedObject.ApplyModifiedProperties();
+// 	}
+
+// 	private void EnvironmentPieceGUI(int index) {
+// 		EditorGUILayout.BeginVertical(GUI.skin.box);
+// 		EditorGUI.indentLevel++;
+
+// 		showEnvironmentPieceSlots[index] = EditorGUILayout.Foldout(showEnvironmentPieceSlots[index], "Environment Piece Slot" + index);
+// 		if (showEnvironmentPieceSlots[index]){
+// 			EditorGUILayout.PropertyField (actionOffsetValuesProperty.GetArrayElementAtIndex(index));
+// 			EditorGUILayout.PropertyField(environmentPiecesProperty.GetArrayElementAtIndex(index));
+			
+// 		}
+
+
+// 		EditorGUI.indentLevel--;		
+// 		EditorGUILayout.EndVertical();
+// 	}
+
+// }
---
+++ Assets/SceneSpecific/FactoryLevel/MagnetHammer/Scripts/MagnetHammerControl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * TODO:
+ * 1. trigger the unlock and drop the hammer. keep track of starting height
+ * 2. detect when the hammer hits the ground. delay for a couple of seconds, then raise the hammer
+ *    triggering the necessary things.
+ * 3. when the hammer reaches the original height, trigger the lockin animation
+ * 
+ */ 
+
+public class MagnetHammerControl : MonoBehaviour, ISynchronizableEnvironmentPiece {
+
+	/*
+	 * Make the magnet hammer drop and go through it's cycle
+	 */
+	public void BeginAction(){
+
+	}
+	/*
+	 * Return the Action Cycle time in GameTime (based on frames, not real time)
+	 */
+	public float ActionCycleTime(){
+		return 0;
+	}
+
+	// Use this for initialization
+	void Start () {
+			
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
